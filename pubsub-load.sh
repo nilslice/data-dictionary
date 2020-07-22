6c11026cc6a4c63f4fab0c16943625fb883710ce
Steve Manuel <nilslice@gmail.com>
2020-07-22 03:40:20 -0600

    script: update pubsub load to run in serial

--- pubsub-load.sh
+++ pubsub-load.sh
@@ -6,5 +6,13 @@ for i in $(seq 1 $1); do
     cp base_partition.txt $PARTITION
 done
 echo "finished writing files... uploading"
-gsutil -m cp base_partition_*.txt gs://nilslice-datasets-dev-public/$2/
+case "$3" in
+	-serial|-s)
+		gsutil cp base_partition_*.txt gs://nilslice-datasets-dev-public/$2/
+		sleep 1
+		;;
+	*)
+		gsutil -m cp base_partition_*.txt gs://nilslice-datasets-dev-public/$2/
+		;;
+esac
 rm base_partition*.txt
