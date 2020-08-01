5d70c2b4de0f9200176eaef9acdce1e813b8cf58
Steve Manuel <nilslice@gmail.com>
2020-08-01 15:04:52 -0600

    script: update pubsub loader tooling to be more customizable

--- pubsub-load.sh
+++ pubsub-load.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "some data" > base_partition.txt
+# ./pubsub-load.sh 50 .json.gz private dataset_name -s
+echo "count of files=$1 file_ext=$2 classification=$3 dataset_name=$4 run_in_serial=$5"
+
+echo "some data" > base_partition$2
 for i in $(seq 1 $1); do
-    PARTITION="base_partition_${i}.txt"
-    cp base_partition.txt $PARTITION
+    PARTITION="base_partition_${i}$2"
+    cp base_partition$2 $PARTITION
 done
 echo "finished writing files... uploading"
-case "$3" in
+case "$5" in
 	-serial|-s)
-		gsutil cp base_partition_*.txt gs://nilslice-datasets-dev-public/$2/
-		sleep 1
+		gsutil cp base_partition_*$2 gs://nilslice-datasets-dev-$3/$4/	
 		;;
 	*)
-		gsutil -m cp base_partition_*.txt gs://nilslice-datasets-dev-public/$2/
+		gsutil -m cp base_partition_*$2 gs://nilslice-datasets-dev-$3/$4/
 		;;
 esac
-rm base_partition*.txt
+rm base_partition*$2
